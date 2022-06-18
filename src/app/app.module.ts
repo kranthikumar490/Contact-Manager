480135114debdabc465e7c7c7c60c49bfeb38c24
Kranthi <Kranthikumar490@outlook.com>
2022-06-18 13:50:23 +0530

    Added the employee dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    EmployeeDashboardComponent
   ],
   imports: [
     BrowserModule,

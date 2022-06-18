ab74ae34c264c676020d5eb6a333aa8ddd0bff4d
Kranthi <Kranthikumar490@outlook.com>
2022-06-18 15:59:39 +0530

    Logic to submit signup form

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  public signupForm !: FormGroup;
+  constructor(private formBuilder: FormBuilder, private http: HttpClient, private router:
+    Router) { }
 
   ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      fullname : [''],
+      email : [''],
+      password: [''],
+      mobile:['']
+    })
+  }
+
+  signUp(){
+    this.http.post<any>("http://localhost:3000/signupUsers", this.signupForm.value).subscribe(res=>{
+      alert("Signup successfully");
+      this.signupForm.reset();
+      this.router.navigate(['login'])
+    }, err=>{
+      alert('something went wrong')
+    })
   }
 
 }
